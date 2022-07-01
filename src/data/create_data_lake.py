46b452f084deaa55f7fe5918416e667038ae0ddc
CarolinaP04 <101909977+CarolinaP04@users.noreply.github.com>
2022-06-30 22:42:27 -0500

    Update create_data_lake.py

--- src/data/create_data_lake.py
+++ src/data/create_data_lake.py
@@ -23,9 +23,12 @@ def create_data_lake():
     """
     import os
 
+    os.mkdir('data_lake')
     os.mkdir('data_lake/landing')
     os.mkdir('data_lake/raw')
-    os.mkdir('data_lake/cleansed')    
+    os.mkdir('data_lake/cleansed')
+    os.mkdir('data_lake/business')
+    os.mkdir('data_lake/business/reports')
     os.mkdir('data_lake/business/reports/figures')
     os.mkdir('data_lake/business/features')
     os.mkdir('data_lake/business/forecasts')
