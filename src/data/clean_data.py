42c3eef7fafae51d50a9ea693bfbad6f48ad0810
CarolinaP04 <101909977+CarolinaP04@users.noreply.github.com>
2022-07-01 23:12:45 -0500

    Update clean_data.py

--- src/data/clean_data.py
+++ src/data/clean_data.py
@@ -12,10 +12,25 @@ def clean_data():
 
 
     """
-    raise NotImplementedError("Implementar esta función")
+    # raise NotImplementedError("Implementar esta función")
 
+    import pandas as pd
+    import os
+    import glob
+
+    origen = os.path.join('data_lake/raw/', "*.csv")
+    list_files = glob.glob(origen)
+
+    df = pd.concat(map(pd.read_csv, list_files), ignore_index=True)
+    df["Fecha"] = pd.to_datetime(df["Fecha"], format="%Y/%m/%d")
+    df2 = pd.melt(df, id_vars=["Fecha"], value_vars = df.columns[1:], var_name= "hora", value_name= "precio")
+    df2["hora"] = df2["hora"].replace({'H':''}, regex=True)
+    df2["hora"] = pd.to_numeric(df2["hora"])
+    df2.to_csv('data_lake/cleansed/precios-horarios.csv',index=False)
+ 
 
 if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
+    clean_data()
