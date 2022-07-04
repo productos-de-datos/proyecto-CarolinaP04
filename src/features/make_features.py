a54e8324d89ed9295fe47e0bdfc61a4880ea1696
CarolinaP04 <cpuertaa@unal.edu.co>
2022-07-03 20:10:53 -0500

    commit model

--- src/features/make_features.py
+++ src/features/make_features.py
@@ -20,9 +20,10 @@ def make_features():
     precios_diarios = pd.read_csv('data_lake/business/precios-diarios.csv')
     precios_diarios['Fecha'] = pd.to_datetime(precios_diarios['Fecha'], format='%Y-%m-%d')
     precios_diarios.sort_values(by = 'Fecha', inplace= True)
-    precios_diarios['log_precio'] = np.log(precios_diarios['precio'])
-    precios_diarios['precio_lag'] = precios_diarios.precio.shift(30)
-    precios_diarios['precio_lag2'] = np.log(precios_diarios['precio_lag'])
+    precios_diarios['precio_lag'] = precios_diarios['precio'].shift(1)
+    precios_diarios['precio_lag10'] = precios_diarios['precio'].shift(10)
+    precios_diarios['precio_lag20'] = precios_diarios['precio'].shift(20)
+    precios_diarios['precio_lag30'] = precios_diarios['precio'].shift(30)
 
     precios_diarios.to_csv('data_lake/business/features/precios-diarios.csv', index=None)
 
--- src/models/train_daily_model.py
+++ src/models/train_daily_model.py
@@ -8,32 +8,33 @@ def train_daily_model():
     """
     # raise NotImplementedError("Implementar esta función")
 
-    import pandas as pd 
-    import numpy as np
-    from sklearn.neural_network import MLPRegressor
+    import pandas as pd
+    from sklearn import linear_model
+    from sklearn.metrics import r2_score
     import pickle
 
-    df_train = pd.read_csv('data_lake/business/features/precios-diarios.csv')
-    df_model = df_train.dropna()
-    X = df_model.iloc[:, -1]
-    X = np.array(X).reshape(-1, 1)
-    y = df_model.iloc[:, 2]
+    precios_diarios = pd.read_csv('data_lake/business/features/precios-diarios.csv', index_col=None)
+    precios_diarios['Fecha'] = pd.to_datetime(precios_diarios['Fecha'], format= '%Y-%m-%d')
+    precios_diarios['anio'] = precios_diarios['Fecha'].dt.year
+    precios_diarios['mes'] = precios_diarios['Fecha'].dt.month 
+    precios_diarios['dia'] = precios_diarios['Fecha'].dt.day
 
+    precios_diarios.dropna(inplace=True)
 
-    H = 1
-    mlp = MLPRegressor(
-        hidden_layer_sizes=(H,),
-        activation="logistic",
-        learning_rate="adaptive",
-        momentum=0.0,
-        learning_rate_init=0.1,
-        max_iter=100000,
-        )
+    x_complete = precios_diarios.copy().drop(columns = 'Fecha')
+    y_complete = x_complete.pop('precio')
 
+    x_train = x_complete[:round(x_complete.shape[0]*0.5)]
+    x_test = x_complete[round(x_complete.shape[0]*0.5):]
+    y_train = y_complete[:round(y_complete.shape[0]*0.5)]
+    y_test = y_complete[round(y_complete.shape[0]*0.5):]
 
-    mlp.fit(X, y)  
+    regression = linear_model.LinearRegression()
+    regression.fit(x_train, y_train)
 
-    pickle.dump(mlp, open('src/models/precios-diarios.pkl', 'wb'))
+    r2_score(y_test,regression.predict(x_test))
+
+    pickle.dump(regression, open('src/models/precios-diarios.pkl', 'wb'))
 
 if __name__ == "__main__":
     import doctest
